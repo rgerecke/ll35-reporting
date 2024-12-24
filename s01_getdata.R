b685b046b3d3f27a0e6f2f698c30f901cd3c77cd
Renata Gerecke <renatavgerecke@gmail.com>
2024-12-23 23:04:01 -0500

    complete drafting `s01_getdata.R`

--- s01_getdata.R
+++ s01_getdata.R
@@ -1,12 +1,64 @@
 # get data from airtable
 
 library(rairtable)
+library(tidyverse)
+
+set_airtable_api_key(Sys.getenv("AIRTABLE_KEY"))
 
 get_ll35_data <- function () {
-  set_airtable_api_key(Sys.getenv("AIRTABLE_KEY"))
-  
-  ll35_at <- airtable(
-    base = "appsmK6xweIG90aYB",
-    table = "tblfXUuSjpWurNZlN"
+  tool_fields <- c(
+    "tool_name",
+    "agency",
+    "department",
+    "tool_desc",
+    "updated",
+    "updated_desc",
+    "month_use",
+    "year_use",
+    "data_training",
+    "data_input",
+    "data_output",
+    "vendor_checkbox",
+    "vendor_name",
+    "vendor_desc",
+    "identifying_info",
+    "population_type",
+    "population_type_individual",
+    "population_type_other",
+    "purpose_type",
+    "purpose_desc",
+    "computation_type"
   )
+
+  ll35_tools <- airtable(
+    base = "appsmK6xweIG90aYB",
+    table = "tblfXUuSjpWurNZlN",
+    view = "Tool Reporting"
+  ) |>
+    read_airtable(fields = tool_fields) |>
+    unnest(agency)
+
+  ll35_agency <- airtable(
+    base = "appsmK6xweIG90aYB",
+    table = "tbl9gC21ohoocjtkJ",
+    view = "All Agencies"
+  ) |>
+    read_airtable(
+      fields = c("Agency Name", "Agency Abbr")
+    ) |>
+    rename(agency_full = "Agency Name",
+           agency_abbr = "Agency Abbr")
+
+  res <- left_join(
+    ll35_tools,
+    ll35_agency,
+    by = c(agency = "airtable_record_id")
+  ) |>
+    mutate(agency = str_glue("{agency_full} ({agency_abbr})"))
+
+  return(res)
 }
+
+ll35_at <- get_ll35_data()
+
+write_rds(ll35_at, "data/df_for_reporting.rds")
